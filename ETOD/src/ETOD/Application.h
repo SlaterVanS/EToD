581961e7326fe8381b45a66e8780a9067ad57242
SlaterVanS Studio <31496326+SlaterVanS@users.noreply.github.com>
2022-02-12 22:50:43 +0800

    Add the OpenGL Shaders tools to ETOD Engine.

--- ETOD/src/ETOD/Application.h
+++ ETOD/src/ETOD/Application.h
@@ -9,6 +9,7 @@
 
 #include "ImGui/ImGuiLayer.h"
 
+#include "ETOD/Renderer/Shader.h"
 
 namespace ETOD {
 
@@ -36,6 +37,7 @@ namespace ETOD {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
