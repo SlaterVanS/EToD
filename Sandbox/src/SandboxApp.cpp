3e0d39f10c643b14ce82861c53030c2569351c7e
SlaterVanS <2238622408@qq.com>
2022-02-19 20:53:47 +0800

    Shader Abstraction and Uniforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -99,7 +99,7 @@ public:
 
 		m_Shader.reset(new ETOD::Shader(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -116,20 +116,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new ETOD::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(new ETOD::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 
@@ -164,13 +166,24 @@ public:
 
 		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				ETOD::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				if (x % 2 == 0)
+				{
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				}
+				else
+				{
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+				}
+				ETOD::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -191,7 +204,7 @@ private:
 	std::shared_ptr<ETOD::Shader> m_Shader;
 	std::shared_ptr<ETOD::VertexArray> m_VertexArray;
 
-	std::shared_ptr<ETOD::Shader> m_BlueShader;
+	std::shared_ptr<ETOD::Shader> m_FlatColorShader;
 	std::shared_ptr<ETOD::VertexArray> m_SquareVA;
 
 	ETOD::OrthographicCamera m_Camera;
