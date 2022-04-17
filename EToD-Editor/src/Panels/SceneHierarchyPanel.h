e9f35e8a0bac901429ee78ebaa190abcbca6148f
SlaterVanS <2238622408@qq.com>
2022-04-17 10:22:13 +0800

    Bulid the Properties Panel to Engine UI.

--- EToD-Editor/src/Panels/SceneHierarchyPanel.h
+++ EToD-Editor/src/Panels/SceneHierarchyPanel.h
@@ -19,6 +19,7 @@ namespace ETOD {
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
