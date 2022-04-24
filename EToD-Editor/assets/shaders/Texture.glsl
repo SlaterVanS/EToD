0dd352751085c2d98fa4290bc8e203dfa3786309
SlaterVanS <2238622408@qq.com>
2022-04-24 22:30:20 +0800

    Preparing Frambuffers for Mouse Picking.

--- EToD-Editor/assets/shaders/Texture.glsl
+++ EToD-Editor/assets/shaders/Texture.glsl
@@ -29,7 +29,7 @@ void main()
 #version 450 core
 
 layout(location = 0) out vec4 color;
-layout(location = 1) out vec4 color2;
+layout(location = 1) out int color2;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
@@ -43,5 +43,5 @@ void main()
 	// EToD: u_TilingFactor
 	color = texture(u_Texture[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
 
-	color2 = vec4( 0.9, 0.2, 0.3, 1.0 );
+	color2 = 50; // placeholder for our entity ID
 }
