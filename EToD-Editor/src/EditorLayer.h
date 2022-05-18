ce57b0f6ff4ccf230f430e2faf7f681a4c1681fd
SlaterVanS <2238622408@qq.com>
2022-05-18 15:07:09 +0800

    Added play/stop button to editor.

--- EToD-Editor/src/EditorLayer.h
+++ EToD-Editor/src/EditorLayer.h
@@ -28,6 +28,11 @@ namespace ETOD {
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		ETOD::OrthographicCameraController m_CameraController;
 
@@ -57,9 +62,18 @@ namespace ETOD {
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 
 }
